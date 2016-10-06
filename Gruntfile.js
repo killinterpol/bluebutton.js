6085300f9815fc07527d555865fc8263417a88fa
Chris Lohl <chris.lohl@cgm.com>
2016-10-05 18:38:32 -0700

    Adding referral parsing
    Signed-off-by: Chris Lohl <chris.lohl@cgm.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,6 +92,7 @@ module.exports = function(grunt) {
           "<%= bb.src %>/parsers/ccda/payers.js",
           "<%= bb.src %>/parsers/ccda/problems.js",
           "<%= bb.src %>/parsers/ccda/procedures.js",
+          "<%= bb.src %>/parsers/ccda/referrals.js",
           "<%= bb.src %>/parsers/ccda/smoking_status.js",
           "<%= bb.src %>/parsers/ccda/vitals.js",
           
